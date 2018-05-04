8bc2e2ee91e1ee8a1693530977f4e5be45f93453
lugope <lucasgpereira.10@gmail.com>
2018-05-03 22:24:02 -0300

    Adding the number of characters of the TOKEN to a constant.

--- token_telegram.py
+++ token_telegram.py
@@ -1,7 +1,9 @@
+TOLKEN_NUMBER_CHARACTERS = 45
+
 def getToken():
 		file = open("TOKEN.txt", 'r')
 
 		# line: string
-		myToken = file.read(45)
+		myToken = file.read(TOLKEN_NUMBER_CHARACTERS)
 
 		return myToken
