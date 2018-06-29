8a1ef2ef3f9189b621c165eca3f851942526730c
lugope <lucasgpereira.10@gmail.com>
2018-06-29 20:36:11 -0300

    Testing get_json_from_url().

--- connection_test.py
+++ connection_test.py
@@ -13,5 +13,14 @@ class URLTest(unittest.TestCase):
 
 		self.assertEqual(expected, result)
 
+	def test_get_json_from_url(self):
+		TOKEN = getToken()
+		URL = "https://api.telegram.org/bot{}/".format(TOKEN)
+
+		expected = {'ok': False, 'error_code': 404, 'description': 'Not Found: method not found'}
+		result = taskbot.get_json_from_url(URL)
+
+		self.assertEqual(expected, result)
+
 if __name__ == "__main__":
 	unittest.main()
